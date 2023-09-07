15855fa8504271dcbf6a168bb8e1c9c5f05712ae
Straple <hadayegor@gmail.com>
2023-09-07 22:56:57 +0300

    add sha256

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
-//
-// Created by haday on 07.09.2023.
-//
+#include <iostream>
+#include "sha256.hpp"
+
+int main() {
+    std::cout << sha256("hello world") << '\n';
+    std::cout << sha256(sha256("hello world")) << '\n';
+}
---
+++ sha256.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+std::string to_hex(unsigned char s);
+
+std::string sha256(const std::string& str);
