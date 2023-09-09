577747e24df84a53c3976a15c9ff48349d32c312
Straple <hadayegor@gmail.com>
2023-09-09 18:26:20 +0300

    update sha256

--- sha256.cpp
+++ sha256.cpp
@@ -1,26 +1,13 @@
 #include "sha256.hpp"
 #include <openssl/sha.h>
-#include <iomanip>
-#include <sstream>
-
-std::string to_hex_symbol(unsigned char s) {
-    std::stringstream ss;
-    ss << std::hex << static_cast<int>(s);
-    return ss.str();
-}
 
 std::string sha256(const std::string &str) {
-    unsigned char hash[SHA256_DIGEST_LENGTH];
+    std::string hash(SHA256_DIGEST_LENGTH, 0);
 
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
     SHA256_Update(&sha256, str.c_str(), str.size());
-    SHA256_Final(hash, &sha256);
+    SHA256_Final(reinterpret_cast<unsigned char *>(hash.data()), &sha256);
 
-    std::stringstream ss;
-    for (unsigned char x : hash) {
-        ss << std::hex << std::setw(2) << std::setfill('0')
-           << static_cast<int>(x);
-    }
-    return ss.str();
+    return hash;
 }
--- sha256.hpp
+++ sha256.hpp
@@ -2,6 +2,4 @@
 
 #include <string>
 
-std::string to_hex_symbol(unsigned char s);
-
-std::string sha256(const std::string& str);
+std::string sha256(const std::string &str);
